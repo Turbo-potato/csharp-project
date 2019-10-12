badcc15ddb30231d707ca16dcbcb6455de6b0d2b
Turbo-potato <diassss178@gmail.com>
2019-10-12 11:04:09 +0600

    lab5

---
+++ WebAppDias/WebAppDias/Controllers/MessagesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppDias.Controllers
+{
+    [Route("Say")]
+    public class MessagesController : Controller
+    {
+         public IActionResult Index()
+         {
+             return View();
+         }
+
+        [Route("{*message}")]
+        public IActionResult ShowMessage(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                ViewData["Message"] = "Message is empty";
+            }
+            else
+            {
+                ViewData["Message"] = message;
+            }
+
+            return View();
+        }
+
+
+    }
+}
